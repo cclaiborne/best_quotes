54447a7fec7f626b35555bbab87684e32a2fa805
cclaiborne <claiborne206@gmail.com>
2014-02-23 17:58:13 -0800

    add chapter 2 files

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,13 +1,6 @@
+# best_quotes/app/controllers/quotes_controller.rb
 class QuotesController < Rulers::Controller
   def a_quote
-    "There is nothing either good or bad " +
-      "but thinking makes it so." +
-      "\n<pre>\n" +
-        "#{env}" +
-      "\n</pre>\n"
-  end
-
-  def exception
-    raise "It's a bad one!"
+    "There is nothing either good or bad " + "but thinking makes it so."
   end
 end
